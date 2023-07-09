ac5ee1af534c9ef83491c8716f847cb3fcfa72e2
Paul Sho <Cyberkiller.CK@gmail.com>
2023-07-09 11:11:08 +0800

    unfinished
    I'll try to get back to this if I'm not busy.

--- train-ticket-system/src/App.jsx
+++ train-ticket-system/src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import './styles/style.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// TopBar component
+const TopBar = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <header className="container header">
+      {/* <!-- ==== NAVBAR ==== --> */}
+      <nav className="nav">
+        <div className="logo">
+          <h2>Shinkansen.</h2>
+        </div>
 
-export default App
+        <div className="nav_menu" id="nav_menu">
+          <button className="close_btn" id="close_btn">
+            <i className="ri-close-fill"></i>
+          </button>
+
+          <ul className="nav_menu_list">
+            <li className="nav_menu_item">
+              <Link to="/account" className="nav_menu_link">
+                account
+              </Link>
+            </li>
+            <li className="nav_menu_item">
+              <Link to="/about" className="nav_menu_link">
+                about
+              </Link>
+            </li>
+            <li className="nav_menu_item">
+              <Link to="/service" className="nav_menu_link">
+                service
+              </Link>
+            </li>
+            <li className="nav_menu_item">
+              <Link to="/contact" className="nav_menu_link">
+                contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <button className="toggle_btn" id="toggle_btn">
+          <i className="ri-menu-line"></i>
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+// LoginForm component
+const LoginForm = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* Add your login form UI code here */}
+    </div>
+  );
+};
+
+// App component
+const App = () => {
+  return (
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route path="/account" element={<LoginForm />} />
+      </Routes>
+      <section className="wrapper">
+        <div className="container">
+          <div className="grid-cols-2">
+            <div className="grid-item-1">
+              <h1 className="main-heading">
+                Welcome to <span>Train Ticket System.</span>
+                <br />
+                Ride Safe.
+              </h1>
+              <p className="info-text">
+                Unlock Your Adventures. Get On Board with Effortless Train Ticketing
+              </p>
+
+              <div className="btn_wrapper">
+                <Link to="/login" className="btn view_more_btn">
+                  Book a ticket <i className="ri-arrow-right-line"></i>
+                </Link>
+
+                <button className="btn documentation_btn">See available Trip</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Router>
+  );
+};
+
+export default App;
---
+++ train-ticket-system/src/client/LoginForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <section className="wrapper">
+      <div className="container">
+        <div className="grid-cols-2">
+          <div className="grid-item-1">
+            <h1 className="main-heading">Login</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <button type="submit" className="btn">
+                Login
+              </button>
+            </form>
+            <p>
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginForm;
